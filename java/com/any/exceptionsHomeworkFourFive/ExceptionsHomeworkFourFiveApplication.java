e279838d9b22fd6dbe039a3871a4eee5f4709d79
dispersion <anyta1901@gmail.com>
2022-02-27 10:52:17 +0300

    Коммит в ветке мастер

---
+++ java/com/any/exceptionsHomeworkFourFive/ExceptionsHomeworkFourFiveApplication.java
@@ -0,0 +1,13 @@
+package com.any.exceptionsHomeworkFourFive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsHomeworkFourFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsHomeworkFourFiveApplication.class, args);
+	}
+
+}
